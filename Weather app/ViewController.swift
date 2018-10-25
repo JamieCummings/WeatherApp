5018a432d411fc6b214f496643bf9edf5a1d75d4
Jamie Cummings <jamiecummings@MacBook-Pro-2.local>
2018-10-25 10:00:21 -0500

    Initial Commit

---
+++ Weather app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather app
+//
+//  Created by Jamie Cummings on 10/25/18.
+//  Copyright © 2018 Jamie Cummings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
