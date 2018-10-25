39951f6502d7b8117a364baee0b900e4eceec0bc
Jamie Cummings <jamiecummings@MacBook-Pro-2.local>
2018-10-25 13:13:15 -0500

    added

--- Weather app/ViewController.swift
+++ Weather app/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let apiKeys = APIKeys()
+        
+        let darkSkyURL = "https://api.darksky.net/forecast/"
+        let darkSkyKey = apiKeys.darkSkyKey
+        let latitude = "37.004842"
+        let longitude = "-85.925876"
+        
+        let url = darkSkyKey + darkSkyURL +  "/" + latitude + "," + longitude
+        
+        let request = Alamofire.request(url)
+        // carrying out our request
+        request.responseJSON { response in
+            // switching based on the result of the request 
+            switch response.result {
+            case .success(let value):
+                // if our request succeeds, take the value and convert it into a JSON object
+                let json = JSON(value)
+                // print out the JSON object
+                print(json)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
     }
 
 
